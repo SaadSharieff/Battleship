d2b3ce51342764b1f1442d357aa1a56a64ff4815
ssharieff <saadsharieff@gmail.com>
2017-08-10 13:10:31 -0500

    created 3 new functions

--- Battleship/Battleship.js
+++ Battleship/Battleship.js
@@ -1,4 +1,18 @@
-
+var view = {
+    displayMessage: function(msg){
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+        
+    },
+    displayHit: function(location){
+        var cell = document.getElementById(location);
+        
+        
+    },
+    displayMiss: function(location){
+        
+    }
+}
 
 
 
